7957eea8eb9ee3cd07129d464d260f0a6bdbfaa5
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-11-08 17:43:04 +0530

    Delete backend directory

--- backend/routes/captureRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { captureContent } from "../controllers/captureController.js";
-const router = express.Router();
-
-router.post("/", captureContent);
-
-export default router;
